d0a2ea4bf2315ab8958e6b9d445c07df98f9db8e
piax787 <ammarfqureshi@gmail.com>
2021-04-13 22:48:29 -0400

    add basic routing and templates for the app

---
+++ medmgmtproject/medreminderapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import home_handler, medications_handler, reminders_handler, history_handler, login_handler
+
+urlpatterns = [
+    path('', home_handler, name='Home'),
+    path('medications', medications_handler, name='Medications'),
+    path('reminders', reminders_handler, name='Reminders'),
+    path('history', history_handler, name='History'),
+    path('login', login_handler, name='Login')
+]
--- medmgmtproject/medreminderapp/views.py
+++ medmgmtproject/medreminderapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the medreminderapp index.")
+
+
+def home_handler(request):
+    context = {
+        'name': 'Julie'
+    }
+    return render(request, 'home.html', context)
+
+
+def medications_handler(request):
+    return render(request, 'medications.html')
+
+
+def reminders_handler(request):
+    return render(request, 'reminders.html')
+
+
+def history_handler(request):
+    return render(request, 'history.html')
+
+
+def login_handler(request):
+    return render(request, 'login.html')
